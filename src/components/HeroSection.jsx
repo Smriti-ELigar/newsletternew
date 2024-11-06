3f077c3ae7e3af3cdc1ddfdec82c77eb850ed408
Smriti-ELigar <smritieligar1005@gmail.com>
2024-11-06 23:22:03 +0530

    added prev styles back

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import { Button } from './Button';
+import './HeroSection.css';
 
 function HeroSection() {
   return (
-    <div className="relative flex flex-col justify-center items-center h-screen w-full bg-black bg-opacity-20">
-      {/* Background Image */}
-      <img
-        src="/heroback.jpg"
-        alt="Background"
-        className="absolute inset-0 w-full h-full object-cover -z-10"
-      />
-
-      {/* Main Heading */}
-      <h1 className="text-white text-[100px] leading-none -mt-24 md:text-[70px] md:-mt-36 sm:text-[50px] sm:-mt-24">
-        SERENITY VIBES
-      </h1>
-
-      {/* Subtitle */}
-      <p className="text-white text-2xl mt-2 md:text-xl sm:text-lg">
-        OCTOBER - 2024
-      </p>
-
-      {/* Buttons Container */}
-      <div className="hero-btns mt-8 flex space-x-3">
+    <div className='hero-container'>
+      <h1>SERENITY VIBES</h1>
+      <p>OCTOBER - 2024</p>
+      <div className='hero-btns'>
         <Button
-          className="btns btn-primary text-lg px-6 py-3"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
           onClick={() => console.log('hey')}
-          to="/cards"
+          to='/cards'
         >
-          VOLUME 34<i className="far fa-play-circle ml-1" />
+          VOLUME 34<i className='far fa-play-circle' />
         </Button>
-        
         <Button
-          className="btns btn-outline text-lg px-6 py-3"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-          to="/team"
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+          to='/team' // assuming this takes to another page
         >
           Vibes Team
         </Button>
